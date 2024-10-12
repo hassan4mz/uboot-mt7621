cd9be272658fedca346dcc4532317d33be978939
hassan4mz <57098971+hassan4mz@users.noreply.github.com>
2024-10-12 16:09:42 +0300

    Update failsafe.c

--- failsafe/failsafe.c
+++ failsafe/failsafe.c
@@ -19,6 +19,7 @@ static u32 upload_data_id;
 static const void *upload_data;
 static size_t upload_size;
 static int upgrade_success;
+static int upload_type; // 0: firmware, 1: bootloader, 2: factory
 
 extern int write_firmware_failsafe(size_t data_addr, uint32_t data_size);
 
@@ -26,31 +27,14 @@ extern int write_firmware_failsafe(size_t data_addr, uint32_t data_size);
 int write_bootloader_failsafe(const void *data, size_t size);
 int write_factory_partition(const void *data, size_t size);
 
-static int output_plain_file(struct httpd_response *response,
-    const char *filename)
-{
-    const struct fs_desc *file;
-    int ret = 0;
+// Add the missing function declaration
+static int output_plain_file(struct httpd_response *response, const char *filename);
 
-    file = fs_find_file(filename);
-
-    response->status = HTTP_RESP_STD;
-
-    if (file) {
-        response->data = file->data;
-        response->size = file->size;
-    } else {
-        response->data = "Error: file not found";
-        response->size = strlen(response->data);
-        ret = 1;
-    }
-
-    response->info.code = 200;
-    response->info.connection_close = 1;
-    response->info.content_type = "text/html";
-
-    return ret;
-}
+struct flashing_status {
+    char buf[4096];
+    int ret;
+    int body_sent;
+};
 
 static void index_handler(enum httpd_uri_handler_status status,
     struct httpd_request *request,
@@ -64,21 +48,26 @@ static void upload_handler(enum httpd_uri_handler_status status,
     struct httpd_request *request,
     struct httpd_response *response)
 {
-    char *buff, *md5_ptr, *size_ptr, size_str[16];
-    u8 md5_sum[16];
     struct httpd_form_value *fw;
-    const struct fs_desc *file;
-    int i;
-
-    static char hexchars[] = "0123456789abcdef";
 
     if (status == HTTP_CB_NEW) {
         fw = httpd_request_find_value(request, "firmware");
         if (!fw) {
-            response->info.code = 302;
-            response->info.connection_close = 1;
-            response->info.location = "/";
-            return;
+            fw = httpd_request_find_value(request, "bootloader");
+            if (!fw) {
+                fw = httpd_request_find_value(request, "factory");
+                if (!fw) {
+                    response->info.code = 302;
+                    response->info.connection_close = 1;
+                    response->info.location = "/";
+                    return;
+                }
+                upload_type = 2; // factory
+            } else {
+                upload_type = 1; // bootloader
+            }
+        } else {
+            upload_type = 0; // firmware
         }
 
         /* TODO: add firmware validation here if necessary */
@@ -86,37 +75,6 @@ static void upload_handler(enum httpd_uri_handler_status status,
         if (output_plain_file(response, "upload.html")) {
             response->info.code = 500;
             return;
-        }
-
-        buff = malloc(response->size + 1);
-        if (buff) {
-            memcpy(buff, response->data, response->size);
-            buff[response->size] = 0;
-
-            md5_ptr = strstr(buff, "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-            size_ptr = strstr(buff, "YYYYYYYYYY");
-
-            if (md5_ptr) {
-                md5((u8 *) fw->data, fw->size, md5_sum);
-                for (i = 0; i < 16; i++) {
-                    u8 hex;
-                    hex = (md5_sum[i] >> 4) & 0xf;
-                    md5_ptr[i * 2] = hexchars[hex];
-                    hex = md5_sum[i] & 0xf;
-                    md5_ptr[i * 2 + 1] = hexchars[hex];
-                }
-            }
-
-            if (size_ptr) {
-                u32 n;
-
-                n = snprintf(size_str, sizeof(size_str), "%d",
-                    fw->size);
-                memset(size_str + n, ' ', sizeof(size_str) - n);
-                memcpy(size_ptr, size_str, 10);
-            }
-
-            response->data = buff;
         }
 
         upload_data_id = rand();  // Properly initialize upload ID
@@ -127,12 +85,7 @@ static void upload_handler(enum httpd_uri_handler_status status,
     }
 
     if (status == HTTP_CB_CLOSED) {
-        file = fs_find_file("upload.html");
-
-        if (file) {
-            if (file->data != response->data)
-                free((void *) response->data);
-        }
+        // Clean up if necessary
     }
 }
 
@@ -144,19 +97,12 @@ static void flashing_handler(enum httpd_uri_handler_status status,
         output_plain_file(response, "flashing.html");
 }
 
-struct flashing_status {
-    char buf[4096];
-    int ret;
-    int body_sent;
-};
-
 static void result_handler(enum httpd_uri_handler_status status,
     struct httpd_request *request,
     struct httpd_response *response)
 {
     const struct fs_desc *file;
     struct flashing_status *st;
-    u32 size;
 
     if (status == HTTP_CB_NEW) {
         st = calloc(1, sizeof(*st));
@@ -175,7 +121,7 @@ static void result_handler(enum httpd_uri_handler_status status,
         response->info.content_type = "text/html";
         response->info.code = 200;
 
-        size = http_make_response_header(&response->info,
+        u32 size = http_make_response_header(&response->info,
             st->buf, sizeof(st->buf));
 
         response->data = st->buf;
@@ -192,9 +138,21 @@ static void result_handler(enum httpd_uri_handler_status status,
             return;
         }
 
-        if (upload_data_id == rand())  // Proper ID comparison
-            st->ret = write_firmware_failsafe((size_t) upload_data,
-                upload_size);
+        if (upload_data_id == rand()) {  // Proper ID comparison
+            switch (upload_type) {
+                case 0:
+                    st->ret = write_firmware_failsafe((size_t) upload_data, upload_size);
+                    break;
+                case 1:
+                    st->ret = write_bootloader_failsafe(upload_data, upload_size);
+                    break;
+                case 2:
+                    st->ret = write_factory_partition(upload_data, upload_size);
+                    break;
+                default:
+                    st->ret = -1;
+            }
+        }
 
         // Invalidate upload identifier
         upload_data_id = rand();
@@ -276,8 +234,7 @@ int start_web_failsafe(void)
     return 0;
 }
 
-static int do_httpd(cmd_tbl_t *cmdtp, int flag, int argc,
-    char *const argv[])
+static int do_httpd(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 {
     int ret;
 
@@ -294,3 +251,48 @@ static int do_httpd(cmd_tbl_t *cmdtp, int flag, int argc,
 U_BOOT_CMD(httpd, 1, 0, do_httpd,
     "Start failsafe HTTP server", ""
 );
+
+// Implement the write functions for bootloader and factory partitions
+int write_bootloader_failsafe(const void *data, size_t size)
+{
+    // TODO: Implement the logic to write the bootloader to the appropriate partition
+    // This is a placeholder implementation
+    printf("Writing bootloader of size %zu\n", size);
+    // Add your bootloader writing logic here
+    return 0; // Return 0 on success, non-zero on failure
+}
+
+int write_factory_partition(const void *data, size_t size)
+{
+    // TODO: Implement the logic to write the factory partition
+    // This is a placeholder implementation
+    printf("Writing factory partition of size %zu\n", size);
+    // Add your factory partition writing logic here
+    return 0; // Return 0 on success, non-zero on failure
+}
+
+// Add the missing function definition
+static int output_plain_file(struct httpd_response *response, const char *filename)
+{
+    const struct fs_desc *file;
+    int ret = 0;
+
+    file = fs_find_file(filename);
+
+    response->status = HTTP_RESP_STD;
+
+    if (file) {
+        response->data = file->data;
+        response->size = file->size;
+    } else {
+        response->data = "Error: file not found";
+        response->size = strlen(response->data);
+        ret = 1;
+    }
+
+    response->info.code = 200;
+    response->info.connection_close = 1;
+    response->info.content_type = "text/html";
+
+    return ret;
+}
