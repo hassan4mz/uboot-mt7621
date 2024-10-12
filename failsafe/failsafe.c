b3247ab02f9bd300e6f836d19e735b5be6b0ae37
hassan4mz <57098971+hassan4mz@users.noreply.github.com>
2024-10-12 16:36:21 +0300

    Update failsafe.c

--- failsafe/failsafe.c
+++ failsafe/failsafe.c
@@ -1,35 +1,74 @@
+/* SPDX-License-Identifier:	GPL-2.0 */
+/*
+ * Copyright (C) 2019 MediaTek Inc. All Rights Reserved.
+ *
+ * Author: Weijie Gao <weijie.gao@mediatek.com>
+ *
+ */
+
+#include <common.h>
+#include <malloc.h>
+#include <net/tcp.h>
+#include <net/httpd.h>
+#include <u-boot/md5.h>
+
+#include "fs.h"
+
+static u32 upload_data_id;
+static const void *upload_data;
+static size_t upload_size;
+static int upgrade_success;
+static char *selected_partition;
+
+extern int write_firmware_failsafe(size_t data_addr, uint32_t data_size);
+extern int write_bootloader_failsafe(size_t data_addr, uint32_t data_size);
+extern int write_factory_failsafe(size_t data_addr, uint32_t data_size);
+
+// ... (keep the existing code)
+
+static void upload_handler(enum httpd_uri_handler_status status,
+    struct httpd_request *request,
+    struct httpd_response *response)
+{
+    char *buff, *md5_ptr, *size_ptr, size_str[16];
+    u8 md5_sum[16];
+    struct httpd_form_value *fw, *partition;
+    const struct fs_desc *file;
+    int i;
+
+    static char hexchars[] = "0123456789abcdef";
+
+    if (status == HTTP_CB_NEW) {
+        fw = httpd_request_find_value(request, "firmware");
+        partition = httpd_request_find_value(request, "partition");
+        if (!fw || !partition) {
+            response->info.code = 302;
+            response->info.connection_close = 1;
+            response->info.location = "/";
+            return;
+        }
+
+        // Store the selected partition
+        selected_partition = strdup(partition->data);
+
+        // ... (keep the existing code)
+
+        return;
+    }
+
+    // ... (keep the existing code)
+}
+
 static void result_handler(enum httpd_uri_handler_status status,
     struct httpd_request *request,
     struct httpd_response *response)
 {
     const struct fs_desc *file;
     struct flashing_status *st;
-    char *result_message;
+    u32 size;
 
     if (status == HTTP_CB_NEW) {
-        st = calloc(1, sizeof(*st));
-        if (!st) {
-            response->info.code = 500;
-            return;
-        }
-
-        st->ret = -1;
-        response->session_data = st;
-        response->status = HTTP_RESP_CUSTOM;
-
-        response->info.http_1_0 = 1;
-        response->info.content_length = -1;
-        response->info.connection_close = 1;
-        response->info.content_type = "text/html";
-        response->info.code = 200;
-
-        u32 size = http_make_response_header(&response->info,
-            st->buf, sizeof(st->buf));
-
-        response->data = st->buf;
-        response->size = size;
-
-        return;
+        // ... (keep the existing code)
     }
 
     if (status == HTTP_CB_RESPONDING) {
@@ -40,78 +79,32 @@ static void result_handler(enum httpd_uri_handler_status status,
             return;
         }
 
-        if (upload_data_id == rand()) {  // Proper ID comparison
-            switch (upload_type) {
-                case 0:
-                    st->ret = write_firmware_failsafe((size_t) upload_data, upload_size);
-                    result_message = st->ret ? "Firmware upgrade failed!" : "Firmware upgrade successful!";
-                    break;
-                case 1:
-                    st->ret = write_bootloader_failsafe(upload_data, upload_size);
-                    result_message = st->ret ? "Bootloader upgrade failed!" : "Bootloader upgrade successful!";
-                    break;
-                case 2:
-                    st->ret = write_factory_partition(upload_data, upload_size);
-                    result_message = st->ret ? "Factory partition upgrade failed!" : "Factory partition upgrade successful!";
-                    break;
-                default:
-                    st->ret = -1;
-                    result_message = "Unknown upgrade type!";
-            }
-        } else {
-            result_message = "Invalid upload session!";
-        }
-
-        // Invalidate upload identifier
-        upload_data_id = rand();
-
-        if (!st->ret)
-            file = fs_find_file("success.html");
-        else
-            file = fs_find_file("fail.html");
-
-        if (!file) {
-            response->data = result_message;
-            response->size = strlen(response->data);
-            return;
-        }
-
-        // Replace placeholder in HTML file with result message
-        char *html_content = malloc(file->size + strlen(result_message));
-        if (html_content) {
-            char *placeholder = strstr(file->data, "{{RESULT_MESSAGE}}");
-            if (placeholder) {
-                size_t prefix_len = placeholder - (char *)file->data;
-                memcpy(html_content, file->data, prefix_len);
-                strcpy(html_content + prefix_len, result_message);
-                strcpy(html_content + prefix_len + strlen(result_message), 
-                       placeholder + strlen("{{RESULT_MESSAGE}}"));
-                
-                response->data = html_content;
-                response->size = strlen(html_content);
+        if (upload_data_id == upload_id) {
+            if (strcmp(selected_partition, "firmware") == 0) {
+                st->ret = write_firmware_failsafe((size_t) upload_data, upload_size);
+            } else if (strcmp(selected_partition, "bootloader") == 0) {
+                st->ret = write_bootloader_failsafe((size_t) upload_data, upload_size);
+            } else if (strcmp(selected_partition, "factory") == 0) {
+                st->ret = write_factory_failsafe((size_t) upload_data, upload_size);
             } else {
-                free(html_content);
-                response->data = file->data;
-                response->size = file->size;
+                st->ret = -1; // Invalid partition
             }
-        } else {
-            response->data = file->data;
-            response->size = file->size;
         }
 
-        st->body_sent = 1;
-
-        return;
+        // ... (keep the existing code)
     }
 
     if (status == HTTP_CB_CLOSED) {
         st = response->session_data;
+
         upgrade_success = !st->ret;
-        if (response->data != st->buf && response->data != fs_find_file("success.html")->data && response->data != fs_find_file("fail.html")->data) {
-            free((void *)response->data);
-        }
+
         free(response->session_data);
+        free(selected_partition);
+        
         if (upgrade_success)
             tcp_close_all_conn();
     }
 }
+
+// ... (keep the remaining code)
